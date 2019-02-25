2b4247e39edb47063f742a51a04f75414b00535c
Justin Cletus <justin.cletus@acquia.com>
2019-02-25 17:19:18 +0530

    implementing facebook and google plus login.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,9 +18,56 @@ class User extends BaseUser
 	* @ORM\GeneratedValue(strategy="AUTO")
 	*/
 	protected $id;
-	
+
 	function __construct()
 	{
 		parent::__construct();
 	}
+
+	/**
+	* @ORM\Column(type="string", name="google_id", nullable=true)
+	*
+	*/
+	protected $googleId;
+
+
+	/**
+	* @ORM\Column(type="string", name="facebook_id", nullable=true)
+	*
+	*/
+	protected $facebookId;
+
+
+	/**
+     * @return mixed
+     */
+    public function getFacebookId()
+    {
+        return $this->facebookId;
+    }
+
+    /**
+     * @param mixed $facebookId
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebookId = $facebookId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGoogleId()
+    {
+        return $this->googleId;
+    }
+
+    /**
+     * @param mixed $googleId
+     */
+    public function setGoogleId($googleId)
+    {
+        $this->googleId = $googleId;
+    }
+
 }
